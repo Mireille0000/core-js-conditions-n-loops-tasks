263f1bdbff8e7554bb357ee3e7482698485936eb
Gasanova Tamara <solas93@mail.ru>
2024-01-06 11:28:02 +0300

    feat: implement isIsoscelesTriangle function

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -91,8 +91,11 @@ function canQueenCaptureKing(/* queen, king */) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if ((a === b || a === c || b === c) && a + b > c) {
+    return true;
+  }
+  return false;
 }
 
 /**
